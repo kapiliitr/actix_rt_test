69d6d63a5b3d2781b3fdc802e7b5b044d1bce49d
Kapil Agarwal <kaagarw@microsoft.com>
2019-04-22 21:27:49 -0700

    Remove dead code

--- src/service.rs
+++ src/service.rs
@@ -2,49 +2,6 @@ use actix_rt::{System, Arbiter};
 use futures::future::{ok, Future, lazy};
 use tokio::runtime::current_thread::Runtime;
 
-pub fn run() {
-    // Uncomment one of the three blocks to run
-
-    /*
-    {
-        let mut sys = System::new("kapiltest");
-
-        sys.block_on(ok::<(), ()>(()).and_then(|()| {
-            println!("{:?}", "system runner hello");
-            Ok(())
-        })).unwrap();
-
-        sys.run().unwrap();
-    }
-    */
-
-    /*
-    {
-        let mut sys = System::new("kapiltest");
-
-        Arbiter::spawn(lazy(|| {
-            println!("{:?}", "arbiter hello");
-            ok(())
-        }));
-
-        sys.run().unwrap();
-    }
-    */
-
-    /*
-    {
-        let mut sys = Runtime::new().unwrap();
-
-        sys.spawn(lazy(|| {
-            println!("{:?}", "tokio hello");
-            ok(())
-        }));
-
-        sys.run().unwrap();
-    }
-    */
-}
-
 pub fn run_sysrunner() {
     {
         let mut sys = System::new("kapiltest");
